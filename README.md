cb4c9124baef57b480f582fc209f152c26059754
Vinay Kumar <tyagivinay807@gmail.com>
2024-06-21 23:34:17 +0530

    Fourth commit

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+![alt text](image-1.png)
+![alt text](image-2.png)
+![alt text](image-3.png)
+![alt text](image-4.png)
+![alt text](image-5.png)
+![alt text](image-6.png)
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,6 @@ import axios from 'axios';  // Import axios for making HTTP requests
 
 const API_KEY = process.env.REACT_APP_NEWS_API_KEY;  // API key for accessing the News API
 const BASE_URL = 'https://newsapi.org/v2';   // Base URL of the News API
-// const BASE_URL = 'https://api.currentsapi.services/v1/';   // Base URL of the News API
 
 // Define a function to fetch articles from the News API
 export const fetchArticles = (category = '', page = 1, query = '') => {
@@ -12,7 +11,7 @@ export const fetchArticles = (category = '', page = 1, query = '') => {
       apiKey: API_KEY,   // Pass the API key as a query parameter
       category, // Specify the category of articles to fetch
       page, // Specify the page number of articles
-      pageSize: 13, // Specify the number of articles per page
+      pageSize: 14, // Specify the number of articles per page
       country: 'in', // Specify the country for which news is to be fetched (India in this case)
       q: query, // Specify a search query for specific articles
     },
